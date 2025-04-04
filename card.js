55e9cc1aa2d9eddb2a1573ccc58e6c4d86bc0d07
kirito <codexstore3@gmail.com>
2025-04-04 17:58:48 +0300

    first commit

---
+++ card.js
@@ -0,0 +1,160 @@
+const balenciagaMainMenu = [
+    {
+        title: 'balenciaga 3xl',
+        images: [
+            "https://balenciaga.dam.kering.com/m/4db4745c94b6ff59/Large-734731W3XL11010_F.jpg?v=3",
+            "https://balenciaga.dam.kering.com/m/17883204e082f5a8/Large-778697W3XLL9191_F.jpg?v=1",
+            "https://balenciaga.dam.kering.com/m/3bfa45de6b09a324/Medium-766034W3XR39710_F.jpg?v=1",
+            "https://balenciaga.dam.kering.com/m/102c767026467f20/Large-814419W3XUA1090_F.jpg?v=1",
+            "https://balenciaga.dam.kering.com/m/10e6ed040d681a1d/Large-734731W2RG39010_F.jpg?v=2"
+        ],
+        href: "https://google.com",
+    },
+    {
+        title: 'balenciaga runner',
+        images: [
+            "https://balenciaga.dam.kering.com/m/584174b2c523b16a/Large-772774W3RBT1000_F.jpg?v=6",
+            "https://balenciaga.dam.kering.com/m/4b34eb8a4d5105cd/Large-772774WRUNG9010_F.jpg?v=1",
+            "https://balenciaga.dam.kering.com/m/379f355b761781fb/Large-813793WRUCU1000_F.jpg?v=1"
+        ],
+        href: "https://google.com",
+    },
+    {
+        title: 'balenciaga track',
+        images: [
+            "https://balenciaga.dam.kering.com/m/614246a71e653806/Large-542436W1GB11000_F.jpg?v=5",
+            "https://balenciaga.dam.kering.com/m/6e1055a4e22cfd55/Large-542436WTRHW9010_F.jpg?v=1",
+            "https://balenciaga.dam.kering.com/m/7b7c9495c7df7055/Large-542436W3AC48071_F.jpg?v=3",
+            "https://balenciaga.dam.kering.com/m/61e97b5971411bf7/Large-690494W3GN11000_F.jpg?v=3"
+
+        ],
+        href: "https://google.com",
+    },
+    {
+        title: 'balenciaga defender',
+        images: [
+            "https://balenciaga.dam.kering.com/m/27ee2e98d9a41441/Large-685613W2RA61000_F.jpg?v=4",
+            "https://balenciaga.dam.kering.com/m/53dc0467b734d93f/Large-685613W2RA69700_F.jpg?v=6"
+        ],
+        href: "https://google.com",
+    },
+    // {
+    //     title: 'balenciaga 10xl',
+    //     images: [
+    //         "https://balenciaga.dam.kering.com/m/560e23f9969803e4/eCom-792779W2MV21000_F.jpg?v=2"
+    //     ],
+    //     href: "https://google.com",
+    // },
+    // {
+    //     title: 'balenciaga cargo',
+    //     images: [
+    //         "https://balenciaga.dam.kering.com/m/614246a71e653806/Large-542436W1GB11000_F.jpg?v=5",
+    //     ],
+    //     href: "https://google.com",
+    // },
+    // {
+    //     title: 'balenciaga 6xl',
+    //     images: [
+    //         "https://balenciaga.dam.kering.com/m/617971650f6142f1/Large-815859W2MV21012_F.jpg?v=1",
+    //         "https://balenciaga.dam.kering.com/m/4f46d9a6f4fa8b25/Large-815859W2MV29000_F.jpg?v=2",
+    //     ],
+    //     href: "https://google.com",
+    // },
+    // {
+    //     title: 'balenciaga triple s',
+    //     images: [
+    //         "https://balenciaga.dam.kering.com/m/3af9cb97f6786d10/Large-533882W09OM1000_F.jpg?v=5",
+    //         "https://balenciaga.dam.kering.com/m/3854d991a1d9606c/Large-524039W2FW19700_F.jpg?v=4",
+    //         "https://balenciaga.dam.kering.com/m/7b7c9495c7df7055/Large-542436W3AC48071_F.jpg?v=3",
+    //         "https://balenciaga.dam.kering.com/m/61e97b5971411bf7/Large-690494W3GN11000_F.jpg?v=3"
+
+    //     ],
+    //     href: "https://google.com",
+    // },
+    // {
+    //     title: 'balenciaga stapler',
+    //     images: [
+    //         "https://balenciaga.dam.kering.com/m/2e0ee84686216d11/Large-803681W3ST41000_F.jpg?v=6",
+    //         "https://balenciaga.dam.kering.com/m/1aad72d936194d08/eCom-803681W3ST49571_F.jpg?v=6",
+    //     ],
+    //     href: "https://google.com",
+    // },
+    // {
+    //     title: 'balenciaga hike',
+    //     images: [
+    //         "https://balenciaga.dam.kering.com/m/5bfd2f3a6a33b201/Large-804921WHIKE1000_F.jpg?v=1",
+    //         "https://balenciaga.dam.kering.com/m/6c722f8913e47b23/Large-804921WHIKE2070_F.jpg?v=2",
+    //     ],
+    //     href: "https://google.com",
+    // },
+    // {
+    //     title: 'balenciaga circuit',
+    //     images: [
+    //         "https://balenciaga.dam.kering.com/m/74a33619c016a3e8/Large-793944WFLGY1000_F.jpg?v=1",
+    //         "https://balenciaga.dam.kering.com/m/2ec73fef5082a501/Large-793944WFLGY9100_F.jpg?v=1",
+    //     ],
+    //     href: "https://google.com",
+    // },
+    // {
+    //     title: 'balenciaga x-pander',
+    //     images: [
+    //         "https://balenciaga.dam.kering.com/m/3af9cb97f6786d10/Large-533882W09OM1000_F.jpg?v=5",
+    //         "https://balenciaga.dam.kering.com/m/3854d991a1d9606c/Large-524039W2FW19700_F.jpg?v=4",
+    //         "https://balenciaga.dam.kering.com/m/7b7c9495c7df7055/Large-542436W3AC48071_F.jpg?v=3",
+    //         "https://balenciaga.dam.kering.com/m/61e97b5971411bf7/Large-690494W3GN11000_F.jpg?v=3"
+
+    //     ],
+    //     href: "https://google.com",
+    // },
+];
+
+const parentContainer = document.querySelector('.mein-menu');
+
+class MainMenu {
+    constructor(title, images, href, parent) {
+        this.title = title;
+        this.images = images;
+        this.parent = parent;
+        this.href = href;
+    }
+
+    render() {
+        const element = document.createElement('article');
+        element.classList.add('list-item' , 'main-list');
+
+        const slides = this.images.map(imgSrc => 
+            `<a href=${this.href} class="swiper-slide"><img src="${imgSrc}" alt="${this.title}" /></a>`
+        ).join('');
+
+        element.innerHTML = `
+            <div class="swiper">
+                <div class="swiper-wrapper">
+                    ${slides}
+                </div>
+                <div class="swiper-pagination"></div>
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+            </div>
+            <h3 class="oswald-medium">${this.title}</h3>
+            <a href=${this.href} class="view-more-btn montserrat-light">more</a>
+        `;
+
+        this.parent.append(element);
+
+        new Swiper(element.querySelector('.swiper'), {
+            direction: 'horizontal',
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    }
+}
+
+balenciagaMainMenu.forEach(item => {
+    new MainMenu(item.title, item.images, item.href, parentContainer).render();
+});
---
+++ gallary.js
@@ -0,0 +1,85 @@
+// gallery.js
+ 
+const goBackBtn = document.createElement('a');
+goBackBtn.href = './balenciaga.html';
+goBackBtn.innerHTML = `
+            <svg fill="#000000" height="50px" width="100px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 242.133 242.133" xml:space="preserve" transform="rotate(-45)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path id="XMLID_9_" d="M237.739,216.526L62.73,41.517l15.91-15.91c4.29-4.29,5.573-10.742,3.252-16.347C79.57,3.655,74.1,0,68.033,0 H15.001c-8.284,0-15,6.716-15,15v53.033c0,6.067,3.655,11.537,9.26,13.858c1.856,0.769,3.805,1.142,5.737,1.142 c3.904,0,7.741-1.524,10.61-4.394l15.91-15.91l175.009,175.01c2.929,2.929,6.768,4.394,10.606,4.394 c3.839,0,7.678-1.464,10.606-4.394C243.597,231.881,243.597,222.384,237.739,216.526z"></path> </g></svg>
+`
+goBackBtn.classList.add('go-back-btn');
+document.querySelector('main').prepend(goBackBtn);
+
+
+
+function updateContent(colorName, commonInfo, colorsData) {
+    const selectedColor = colorsData.find((item) => item.colorName === colorName);
+    
+    if (selectedColor) {
+        // Обновляем галерею изображений
+        new MainMenu(selectedColor.colorName, selectedColor.images).render();
+        
+        // Обновляем текстовую информацию
+        const itemInfo = document.querySelector(".item-info");
+        itemInfo.querySelector("h2").textContent = selectedColor.name;
+        itemInfo.querySelector(".descr p:nth-child(1)").textContent = "Price:";
+        itemInfo.querySelector(".descr ul").innerHTML = `
+            <li>Excellent Quality - ${selectedColor.prices.excellent}</li>
+            <li>Good Quality - ${selectedColor.prices.good}</li>
+            <li>Middle Quality - ${selectedColor.prices.middle}</li>
+        `;
+        itemInfo.querySelector(".descr p:nth-child(3)").textContent = `Sizes: ${commonInfo.sizes}`;
+        itemInfo.querySelector(".descr p:nth-child(4)").textContent = commonInfo.shipping;
+    }
+}
+
+// Функция для смены изображения галереи
+class MainMenu {
+    constructor(colorName, images) {
+        this.colorName = colorName;
+        this.images = images;
+    }
+
+    render() {
+        const parentContainer = document.querySelector(".galary");
+        parentContainer.innerHTML = `
+            <div>
+                <a data-fancybox="gallery" data-src="${this.images[0]}">
+                    <img src="${this.images[0]}" alt="" />
+                </a>
+            </div>
+            <div>
+                ${this.images
+                    .slice(1)
+                    .map(
+                        (img) => `
+                    <a data-fancybox="gallery" data-src="${img}">
+                        <img src="${img}" alt="" />
+                    </a>
+                `)
+                    .join("")}
+            </div>
+        `;
+        Fancybox.bind('[data-fancybox="gallery"]', {});
+    }
+}
+
+// Добавление обработчиков для смены цветов
+function setColorButtons(colorsData, commonInfo) {
+    const colorButtonsContainer = document.querySelector(".colors");
+    colorButtonsContainer.innerHTML = colorsData.map((color) => `
+        <button class="${color.colorName}"></button>
+    `).join('');
+
+    const colorButtons = document.querySelectorAll(".colors button");
+
+    colorButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            updateContent(button.classList[0], commonInfo, colorsData);  // Класс кнопки соответствует названию цвета
+        });
+    });
+}
+
+// Инициализация при загрузке страницы
+function initializePage(colorsData, commonInfo) {
+    updateContent("black", commonInfo, colorsData);  // Показываем первую галерею по умолчанию
+    setColorButtons(colorsData, commonInfo);
+}
