1153ccf56f247bc06ea1c9c5363ca674deaa1567
kirito <codexstore3@gmail.com>
2025-04-12 00:21:52 +0300

    goaushjdzfasodsfgdhasdasahdg

--- gallary.js
+++ gallary.js
@@ -2,12 +2,32 @@
  
 const goBackBtn = document.createElement('button');
 goBackBtn.innerHTML = `
-            <svg fill="#000000" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 476.213 476.213" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <polygon points="476.213,223.107 57.427,223.107 151.82,128.713 130.607,107.5 0,238.106 130.607,368.714 151.82,347.5 57.427,253.107 476.213,253.107 "></polygon> </g></svg>
-`
+<svg fill="#000000" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" 
+xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 476.213 476.213" 
+xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+<g id="SVGRepo_iconCarrier"> 
+<polygon points="476.213,223.107 57.427,223.107 151.82,128.713 130.607,107.5 
+0,238.106 130.607,368.714 151.82,347.5 57.427,253.107 476.213,253.107 "></polygon> 
+</g></svg>
+`;
+
 goBackBtn.classList.add('go-back-btn');
+
 goBackBtn.addEventListener('click', () => {
-    history.back();
+    const referrer = document.referrer;
+    
+    if (referrer && new URL(referrer).origin === location.origin) {
+        // Переход на предыдущую страницу без параметров
+        const cleanURL = new URL(referrer);
+        cleanURL.search = ''; // удаляем query-параметры
+        location.href = cleanURL.href;
+    } else {
+        // Переход на главную, если предыдущей страницы нет
+        location.href = './../index.html';
+    }
 });
+
 document.querySelector('main').prepend(goBackBtn);
 
 
